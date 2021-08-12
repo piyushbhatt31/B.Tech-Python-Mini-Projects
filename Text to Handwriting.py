fee694e5049177ed1927f9222d8a0f8ccfa094ad
Piyush Bhatt <65225238+kingpiyush3101@users.noreply.github.com>
2021-08-12 17:19:53 +0530

    Update Text to Handwriting.py

--- Text to Handwriting.py
+++ Text to Handwriting.py
@@ -1,5 +1,9 @@
 import pywhatkit
+import cv2
 print("<<<----------- Text to Handwriting ---------->>>")
 a=input("Enter Text : ")
-pywhatkit.text_to_handwriting(a, rgb=(0, 0, 255))
-print("Saved!")
+pywhatkit.text_to_handwriting(a, rgb=(0, 0, 255), save_to="abc.png")
+img=cv2.imread("abc.png")
+cv2.imshow("Converted",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
